d33f5db78f96f23a67eebdcea2e4671173c3861a
leekj601 <133008364+leekj601@users.noreply.github.com>
2023-07-29 11:45:16 +0900

    최종본

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import dlib
 from flask import Flask, render_template, Response, redirect
 import time
 import webbrowser
+import threading
+import speech_recognition as sr
 
+recognizer = sr.Recognizer()
 app = Flask(__name__)
 
 # Dlib의 눈동자 추적기와 얼굴 검출기 초기화
@@ -45,10 +48,6 @@ def track_eye(frame):
         if prev_right_eye_center != (0, 0):
             cv2.line(frame, prev_right_eye_center, right_eye_center, (0, 0, 255), 2)
 
-        # 눈동자 좌표 출력
-        print("Left Eye:", left_eye_center)
-        print("Right Eye:", right_eye_center)
-
         # 눈 감김 감지
         left_eye_aspect_ratio = calculate_eye_aspect_ratio([shape.part(i) for i in range(36, 42)])
         right_eye_aspect_ratio = calculate_eye_aspect_ratio([shape.part(i) for i in range(42, 48)])
@@ -65,8 +64,8 @@ def track_eye(frame):
                 blink_end_time = time.time()
                 blink_duration = blink_end_time - blink_start_time
                 print("Blink Duration:", blink_duration)
-                if blink_duration>=2:
-                     webbrowser.open('https://www.naver.com', new=2)
+                if blink_duration>=5:
+                     cv2.putText(frame, "휴식을 취하세요", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
                 blink_start_time = None  # 초기화하여 다음 눈 감김을 기록할 수 있도록 함
 
         # 이전 눈동자 좌표 업데이트
@@ -89,6 +88,37 @@ def calculate_eye_aspect_ratio(eye_points):
 
     return aspect_ratio
 
+is_speech_recognition_enabled = True
+
+# 음성 인식 스레드
+def speech_recognition_thread():
+    global is_speech_recognition_enabled
+
+    while True:
+        if is_speech_recognition_enabled:
+            with sr.Microphone() as source:
+                print("음성을 입력하세요...")
+                recognizer.adjust_for_ambient_noise(source)  # 주변 소음에 적응
+                audio = recognizer.listen(source)
+
+            try:
+                # 음성을 텍스트로 변환
+                command = recognizer.recognize_google(audio, language='ko-KR')
+                print("음성 명령: " + command)
+
+                # 특정 명령어(여기서는 '네이버')를 인식하면 원하는 사이트 열기
+                if '네이버' in command:
+                    webbrowser.open('https://www.naver.com', new=2)
+
+            except sr.UnknownValueError:
+                print("음성을 인식하지 못했습니다.")
+            
+            except sr.RequestError as e:
+                print("Google Speech Recognition 서비스에 접근할 수 없습니다. 오류: {0}".format(e))
+
+            # 음성 인식 후 잠시 대기
+            time.sleep(5)
+
 # Flask 라우트: 눈동자 추적 페이지
 @app.route('/')
 def index():
@@ -96,6 +126,15 @@ def index():
 
 # Flask 라우트: 웹캠 스트리밍 페이지
 def gen_frames():
+    global is_speech_recognition_enabled
+
+    # 음성 인식 스레드 시작
+    speech_thread = threading.Thread(target=speech_recognition_thread)
+    speech_thread.start()
+
+    speech_recognition_interval = 5  # 음성 인식 간격 (초 단위)
+    last_speech_recognition_time = time.time()
+
     while True:
         success, frame = video_capture.read()
         if not success:
@@ -105,12 +144,24 @@ def gen_frames():
 
             ret, buffer = cv2.imencode('.jpg', frame_with_eyes)
             frame = buffer.tobytes()
+
+            # 음성 인식을 수행할 시간인지 확인
+            current_time = time.time()
+            if current_time - last_speech_recognition_time >= speech_recognition_interval:
+                # 'speech_recognition_interval' 초마다 음성 인식 수행
+                is_speech_recognition_enabled = True
+                last_speech_recognition_time = current_time
+
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+            # 음성 인식 상태를 다시 비활성화
+            is_speech_recognition_enabled = False
+
 
 @app.route('/video_feed')
 def video_feed():
     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
